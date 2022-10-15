67fd428b8206c53e81632442b141f4e043ec73f0
Bhavna Matwani <53350565+bhavna-matwani@users.noreply.github.com>
2022-10-15 19:45:07 -0400

    Update prob99.ml

--- prob99.ml
+++ prob99.ml
@@ -6,4 +6,10 @@ let rec last list =
   | [tl] -> [tl]
   | hd::tl -> last tl;;
   
-  
+ (* 2. Write a function last : 'a list -> 'a list that returns the last two elements of a list*) 
+
+let rec last2 list = 
+  match list with
+  | [] -> []
+  | [x;y] -> [x;y]
+  | hd::tl -> last2 tl;;
